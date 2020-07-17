34c2c4e50697d5c0c0626e656fc21b6167665bf6
Moshow <withyouamalive@gmail.com>
2020-07-17 02:00:21 +0100

    resolving issue

--- src/Components/elements/Routes.js
+++ src/Components/elements/Routes.js
@@ -1,4 +1,4 @@
-const base_url = 'http://127.0.0.1:8000/';
+const base_url = 'http://127.0.0.1:8000/'; 
 export const user_registration_url = `${base_url}api/v1/users/signup`; 
 export const user_login_url = `${base_url}api/v1/users/login`;
 export const create_event_url = `${base_url}api/v1/users/create_event`;
